29f0e756b7d08689dd692d0360766bf79bd5bdc3
kbadal19 <k.badal19@gmail.com>
2023-08-30 14:38:12 +0530

    carousel autofade working and search implemented

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,19 +1,24 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import Card from "../components/card";
+import Carousel from "../components/carousel";
 
 export default function Home() {
   return (
     <div>
-        <div>
-            <Navbar/>
-        </div>
-        <div>
-            Body
-        </div>
-        <div>
-            <Footer/>
-        </div>
+      <div>
+        <Navbar />
+      </div>
+      <div>
+        <Carousel />
+      </div>
+      <div>
+        <Card />
+      </div>
+      <div>
+        <Footer />
+      </div>
     </div>
-  )
+  );
 }
